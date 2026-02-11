6742e2ebf1eed94730de56ab87a9aaa7dda1cf94
HomePC <adedoyin.adedara@gmail.com>
2026-02-11 19:44:55 +0100

    Added three sounds of each drums in the drums challenge. the challenge is still going on

--- 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
+++ 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
@@ -11,10 +11,29 @@ let i = 0;
 let numberOfDrum = document.querySelectorAll(".drum").length;
 
 // putting the while condition 
-while (i <= numberOfDrum){
+while (i < numberOfDrum){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        let audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
+        let drumSound = this.innerHTML;
+        switch (drumSound) {
+            case "w":
+                let tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();                
+                break;
+            case "a":
+                let tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();                
+                break;
+            case "s":
+                let tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+        
+            default:
+                console.log(this.innerHTML)
+        }
     });
     i++;
 }
+
+
+// let audio = new Audio("./sounds/tom-1.mp3");
+//         audio.play();
