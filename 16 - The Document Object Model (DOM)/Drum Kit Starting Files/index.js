8cf19b7c40a5374eb80c06c0cbb8d3b9b7fe6384
HomePC <adedoyin.adedara@gmail.com>
2026-02-11 22:28:29 +0100

    was able to add the keypress listerner to the drum project

--- 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
+++ 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
@@ -6,34 +6,65 @@
 
 // using the concept of loop and anonimous function for the challenge 
 // let's create a function using while loop
+
+// this takes the click button listener
 let i = 0;
 
-let numberOfDrum = document.querySelectorAll(".drum").length;
+// the the DOM for the drum be given a variable 
+let drumEvent = document.querySelectorAll(".drum")
 
 // putting the while condition 
-while (i < numberOfDrum){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        let drumSound = this.innerHTML;
-        switch (drumSound) {
+while (i < drumEvent.length){
+    drumEvent[i].addEventListener("click", function (){
+        let buttonInnerHTML = this.innerHTML;
+      makeSound(buttonInnerHTML);
+    });
+    i++;
+}
+// this takes the Keybaord listener
+document.addEventListener("keypress", function (){
+    makeSound(event.key);
+});
+
+
+function makeSound(key){
+      switch (key) {
             case "w":
                 let tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();                
                 break;
+
             case "a":
                 let tom2 = new Audio("./sounds/tom-2.mp3");
                 tom2.play();                
                 break;
+
             case "s":
                 let tom3 = new Audio("./sounds/tom-3.mp3");
                 tom3.play();
+                break;
+
+        case "d":
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play()
+            break;
+        case "j":
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
         
+        case "l":
+            let kickBass = new Audio("./sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
             default:
                 console.log(this.innerHTML)
         }
-    });
-    i++;
 }
-
-
 // let audio = new Audio("./sounds/tom-1.mp3");
 //         audio.play();
