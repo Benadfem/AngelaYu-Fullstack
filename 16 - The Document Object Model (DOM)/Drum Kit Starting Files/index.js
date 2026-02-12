0c9feb9f14b5c858f805c92f87263debe1045cdb
HomePC <adedoyin.adedara@gmail.com>
2026-02-12 17:36:18 +0100

    the practice code is successfully done. i was able to revise the addEventListener method and perform some other things in the project/ thanks angela yu for this course

--- 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
+++ 16 - The Document Object Model (DOM)/Drum Kit Starting Files/index.js
@@ -30,7 +30,7 @@ document.addEventListener("keydown", function (event){
 });
 
 
-function makeSound(key){
+function makeSound(){
       switch (key) {
             case "w":
                 let tom1 = new Audio("./sounds/tom-1.mp3");
--- PRACTICE EVENT LISTENER/index.js
+++ PRACTICE EVENT LISTENER/index.js
@@ -1,9 +1,50 @@
-let button = document.querySelectorAll(".button")
+let button = document.querySelectorAll(".button");
 
-for (let i = 0; i < button.length, i++;){
-    button[i].addEventListener("click", clickHere);
+// create a variable to hold the DOM for the h1
+let h1 = document.querySelector("h1");
+
+for (let i = 0; i < button.length; i++){
+    button[i].addEventListener("click", function (){
+        let buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "RED":
+                h1.style.color = "red";
+                document.querySelector(".red").style.color = "red";
+                changeFont();
+                break;
+
+            case "BLUE":
+                h1.style.color = "blue";
+                changeFont();
+                document.querySelector(".blue").style.color = "blue";
+                break;
+
+            case "YELLOW":
+                h1.style.color = "yellow";
+                document.querySelector(".yellow").style.color = "yellow";
+                changeFont();
+                break;
+
+            case "PINK":
+                h1.style.color = "pink";
+                changeFont();
+                document.querySelector(".pink").style.color = "pink";
+                break;
+
+            default:
+                console.log("This is not acceptable ");
+                break;
+}
+    });
 }
 
-function clickHere(){
-    alert("I have clicked ");
+// function that modifies the style for the button click
+function changeFont(){
+    h1.classList.add("height");
+     setTimeout(() => {
+         h1.classList.remove("height");
+             }, 1000);
 }
+
+
+
